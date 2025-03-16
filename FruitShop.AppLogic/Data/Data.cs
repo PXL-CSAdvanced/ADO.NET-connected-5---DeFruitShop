8267d88567ba8a1b07e81511aa8e2744ffa94c88
Wim Bervoets <wim.bervoets@pxl.be>
2025-03-16 21:53:15 +0100

    Add project files.

---
+++ FruitShop.AppLogic/Data/Data.cs
@@ -0,0 +1,33 @@
+﻿using FruitShop.AppLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitShop.AppLogic.Data
+{
+    static class Data
+    {
+        public static int CreateFruit(Fruit fruit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int UpdateFruit(Fruit fruit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int DeleteFruit(Fruit fruit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Fruit> GetAllFruit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FruitShop.AppLogic/Models/Fruit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitShop.AppLogic.Models
+{
+    class Fruit
+    {
+        public int Id { get; set; }
+        
+        public Fruit()
+        {
+                
+        }
+    }
+}
